32540b9e61c8ae1dad43e409cdab6d2da10d25f2
Mmarafa <mahmoodmmarafa36@gmail.com>
2022-09-22 03:38:24 -0700

    Commit 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,1 +1,26 @@
+#include"main.h"
 
+/**
+ * print_number - Prints an integer.
+ *
+ * @n: The integer to be printed.
+*/
+
+void print_number(int n)
+{
+	unsigned int no = n;
+
+	/*first check if its negative*/
+	if (n < 0)
+	{
+		_putchar('-');
+		no = -no;
+	}
+
+	/*print the first few digits*/
+	if ((no / 10) > 0)
+		print_number(no / 10);
+
+	/*print the last digit*/
+	_putchar((no % 10) + 48);
+}
