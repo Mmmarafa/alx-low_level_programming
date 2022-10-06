12d8f9860d45b5fe3f38c3c02a392a8acb1bc8f4
Mmarafa <mahmoodmmarafa36@gmail.com>
2022-10-06 03:51:17 -0700

    my commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdib.h>
+
+/**
+* malloc_checked - allocates memoy using malloc
+*
+* @b: amount 0f bytes
+*
+* Return: pointer to our new allocated memory exit with 98 if malloc fails
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *new_mem;
+
+	new_mem = malloc(b);
+	if (new_mem == NULL)
+		exit(98);
+	return (new_mem);
+}
